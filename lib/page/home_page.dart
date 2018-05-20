d399d24f84bab4e201e2a5ee0c4a6a079cafb71b
sun <asmh1989@gmail.com>
2018-05-20 11:14:11 +0800

    首页界面

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -13,6 +13,17 @@ import 'package:after_layout/after_layout.dart';
 
 var _textStyle = new TextStyle(color: Colors.white, fontSize: 16.0);
 
+
+class _Menu {
+  final String image;
+  final String title;
+
+  _Menu({
+    this.image,
+    this.title
+  });
+}
+
 class MainPage extends StatefulWidget{
   @override
   _MainPageState createState() => new _MainPageState();
@@ -20,71 +31,119 @@ class MainPage extends StatefulWidget{
 
 class _MainPageState extends State<MainPage> with AfterLayoutMixin<MainPage>  {
 
+  List<_Menu> _menus = <_Menu>[];
 
   void openSettings(){
     Navigator.push(context, new MaterialPageRoute(builder: (_) => new SettingsPage()));
+  }
+
+  Widget getMenu(_Menu menu){
+    return new Container(
+        height: 160.0,
+        margin: new EdgeInsets.all(12.0),
+        child: new RaisedButton(
+          onPressed: (){},
+          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+          color: Colors.white,
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Image.asset(menu.image, height: 100.0),
+              new SizedBox(height: 9.0,),
+              Text(menu.title, style: TextStyle(fontSize: 16.0))
+            ],
+          ),
+        ));
+  }
+
+  List<TableRow> getTable() {
+    List<TableRow> list = <TableRow>[];
+    for(int i = 0, len = _menus.length; i< len; i+= 2){
+
+      list.add(new TableRow(
+          children: <Widget>[
+            getMenu(_menus[i]),
+            getMenu(_menus[i+1]),
+          ]
+      ));
+    }
+
+    return list;
   }
 
   List<Widget> buildSliverList(){
     UserInfo _user = getUserInfo(context);
 
     List<Widget> list = new List<Widget>();
+    final Orientation orientation = MediaQuery.of(context).orientation;
+
 
     list.add(
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: 200.0,
-          padding: new EdgeInsets.only(bottom: 40.0),
-          decoration: new BoxDecoration(
-              image: new DecorationImage(image: new AssetImage(ImageAssets.home_background), fit: BoxFit.cover)
-          ),
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                height: 64.0,
-                padding: new EdgeInsets.all(2.0),
-                decoration: new BoxDecoration(
+        new Stack(children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 200.0,
+            padding: new EdgeInsets.only(bottom: 40.0),
+            decoration: new BoxDecoration(
+                image: new DecorationImage(image: new AssetImage(ImageAssets.home_background), fit: BoxFit.cover)
+            ),
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 64.0,
+                  padding: new EdgeInsets.all(2.0),
+                  decoration: new BoxDecoration(
 //                    image: new DecorationImage(image: new AssetImage(ImageAssets.login_user)),
-                    color: Colors.blue,
-                    shape: BoxShape.circle
+                      color: Colors.blue,
+                      shape: BoxShape.circle
+                  ),
+                  child: Icon(Icons.person, size: 60.0, color: Colors.white,),
                 ),
-                child: Icon(Icons.person, size: 60.0, color: Colors.white,),
-              ),
-              new SizedBox(height: 5.0,),
-              Text(_user?.displayname??' ', style: TextStyle(fontSize: 22.0, color: Colors.white),),
-              new SizedBox(height: 5.0,),
+                new SizedBox(height: 5.0,),
+                Text(_user?.displayname??' ', style: TextStyle(fontSize: 22.0, color: Colors.white),),
+                new SizedBox(height: 5.0,),
 
-              new InkWell(
-                  onTap: (){
-                    Navigator.push(context, new MaterialPageRoute(builder: (_)=> new TaskPage()));
-                  },
-                  child: new Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      new Image.asset(ImageAssets.home_message, width: 18.0,),
-                      Text('您尚有', style: _textStyle,),
-                      Material(color: Colors.transparent,
-                        elevation: 2.0,
-                        child: new Container(
-                          height: 20.0,
-                          width: 24.0,
-                          decoration: new BoxDecoration(color: const Color(0xFFFF232D), borderRadius: new BorderRadius.circular(5.0)),
-                          padding: EdgeInsets.all(2.0),
-                          child: Text('${_user?.orders??0}', style: _textStyle, textAlign: TextAlign.center,),
+                new InkWell(
+                    onTap: (){
+                      Navigator.push(context, new MaterialPageRoute(builder: (_)=> new TaskPage()));
+                    },
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        new Image.asset(ImageAssets.home_message, width: 18.0,),
+                        Text('您尚有', style: _textStyle,),
+                        Material(color: Colors.transparent,
+                          elevation: 2.0,
+                          child: new Container(
+                            height: 20.0,
+                            width: 24.0,
+                            decoration: new BoxDecoration(color: const Color(0xFFFF232D), borderRadius: new BorderRadius.circular(5.0)),
+                            padding: EdgeInsets.all(2.0),
+                            child: Text('${_user?.orders??0}', style: _textStyle, textAlign: TextAlign.center,),
+                          ),
                         ),
-                      ),
-                      Text('项任务未处理', style: _textStyle,),
-                      Icon(Icons.navigate_next, color: Colors.white,)
-                    ],
-                  ))
+                        Text('项任务未处理', style: _textStyle,),
+                        Icon(Icons.navigate_next, color: Colors.white,)
+                      ],
+                    )),
 
-            ],
+              ],
+            ),
           ),
-        )
+          new Container(
+            margin: new EdgeInsets.only(top: 150.0),
+            width: MediaQuery.of(context).size.width,
+            child: new Table(
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              children: getTable(),
+            ),
+
+          )
+        ])
     );
     return list;
   }
@@ -92,18 +151,28 @@ class _MainPageState extends State<MainPage> with AfterLayoutMixin<MainPage>  {
   @override
   void initState() {
     super.initState();
+
+    _menus.add(new _Menu(image: ImageAssets.home_order, title: '工单邮箱'));
+    _menus.add(new _Menu(image: ImageAssets.home_assets, title: '资产扫描'));
+    _menus.add(new _Menu(image: ImageAssets.home_material, title: '库存查询'));
+    _menus.add(new _Menu(image: ImageAssets.home_history, title: '历史记录'));
+    _menus.add(new _Menu(image: ImageAssets.home_notification, title: '通知公告'));
+    _menus.add(new _Menu(image: ImageAssets.home_meter, title: '仪表抄表'));
+
   }
 
 
 
   @override
   Widget build(BuildContext context) {
+    getUserInfo(context) ??_handlerRefresh();
+
     return new Scaffold(
 
       body: new RefreshIndicator(
           onRefresh: _handlerRefresh,
           child: new CustomScrollView(
-            physics: new AlwaysScrollableScrollPhysics(),
+              physics: new AlwaysScrollableScrollPhysics(),
               slivers: <Widget>[
                 SliverAppBar(
                   title: const Text('深水光明移动工单系统'),
@@ -159,5 +228,11 @@ class _MainPageState extends State<MainPage> with AfterLayoutMixin<MainPage>  {
   @override
   void afterFirstLayout(BuildContext context) {
     _handlerRefresh();
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+//    _handlerRefresh();
   }
 }
--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -5,4 +5,11 @@ class ImageAssets {
   static const String home_background = 'assets/images/home_background.png';
   static const String home_message = 'assets/images/home_message.png';
 
+  static const String home_assets = 'assets/images/home_assets.png';
+  static const String home_notification = 'assets/images/home_notification.png';
+  static const String home_history = 'assets/images/home_history.png';
+  static const String home_order = 'assets/images/home_order.png';
+  static const String home_material = 'assets/images/home_material.png';
+  static const String home_meter = 'assets/images/home_meter.png';
+
 }
