93c8159f5804ee960c22bd1b18c6318f3beee9cb
sun <asmh1989@gmail.com>
2018-06-19 16:57:36 +0800

    一键上传ios脚本

--- publish.sh
+++ publish.sh
@@ -1,17 +1,66 @@
 #! /bin/sh
 
+pwd=`pwd`
+
 uKey=9812388432113be529db985e3ba38224
 _api_key=7985c2827257ea59c9559e929a880dd9
 
-echo "build android ...."
+scheme='Runner'
+info_plist=${pwd}/ios/Runner/info.plist
+workspace=Runner.xcworkspace
 
-flutter build apk --release -t lib/main_publish.dart
+uploadAndroid(){
+    flutter build apk --release -t lib/main_publish.dart
 
-pwd=`pwd`
+    apk_release_file=${pwd}/build/app/outputs/apk/release/app-release.apk
 
-apk_release_file=${pwd}/build/app/outputs/apk/release/app-release.apk
+    echo "开始上传 ${apk_release_file} 到蒲公英...."
+    curl -F "file=@${apk_release_file}" -F "_api_key=${_api_key}" https://www.pgyer.com/apiv2/app/upload
 
-echo "开始上传 ${apk_release_file} 到蒲公英...."
-curl -F "file=@${apk_release_file}" -F "_api_key=${_api_key}" https://www.pgyer.com/apiv2/app/upload
+    echo "\n android done"
+}
 
-echo "\n android done"
+uploadIos() {
+
+    configurationBuildDir="`pwd`/build"
+
+    archivePath="${configurationBuildDir}/${scheme}.xcarchive"
+    exportOptionsPlist="`pwd`/AdHocExportOptions.plist"
+    exportPath="${configurationBuildDir}";
+
+    version=`/usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' ${info_plist}`
+
+    cd ${pwd}/ios
+
+    newVersion=${version}.`date +%y%m%d%H%M`
+
+    xcodebuild clean -configuration "Release"
+
+    /usr/libexec/PlistBuddy ${info_plist} -c "Set CFBundleShortVersionString ${newVersion}"
+
+    xcodebuild archive -workspace "${workspace}" -scheme "${scheme}" -configuration "Release" -archivePath "${archivePath}"
+
+    xcodebuild -exportArchive -archivePath "${archivePath}" -exportOptionsPlist "${exportOptionsPlist}" -exportPath "${exportPath}"
+
+    cd ${pwd}
+
+    git checkout -- ios
+
+    ios_build_file=${pwd}/build/Runner.ipa
+
+    ios_release_file=${pwd}/build/Samex${newVersion}_`git log --oneline --pretty=format:"%h" | head -n 1`.ipa
+
+    cp ${ios_build_file} ${ios_release_file}
+
+    echo "开始上传 ${ios_release_file} 到蒲公英...."
+    curl -F "file=@${ios_release_file}" -F "_api_key=${_api_key}" https://www.pgyer.com/apiv2/app/upload
+}
+
+if [ "$1" = "ios" ]; then
+    echo "build ios release ..."
+    flutter build ios --release -t lib/main_publish.dart
+    uploadIos
+else
+    echo "build android release ..."
+    uploadAndroid
+fi
