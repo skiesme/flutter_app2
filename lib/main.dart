94611b89b4a1dc85c7392f8f59a52318409b8eeb
wangzhen <wangzhen@chinahdkj.com>
2019-01-24 10:03:33 +0800

    [chore]update libs

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ import 'package:samex_app/data/badge_bloc.dart';
 void main() async {
   await Cache.getInstance();
   runApp(new App());
-
 }
 
 class App extends StatelessWidget {
@@ -99,14 +98,6 @@ class _MyAppState extends State<MyApp> {
           // ... other locales the app supports
         ],
         theme: new ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-          // counter didn't reset back to zero; the application is not restarted.
           primaryColor: Style.primaryColor,
           textTheme: theme.textTheme.copyWith(
             body1: theme.textTheme.body1.copyWith(
