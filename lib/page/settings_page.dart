9421225cce5ba623fb7903c803e98c412bdcc4f8
wangzhen <wangzhenzy@outlook.com>
2019-10-21 14:55:51 +0800

    [Update] app version.

--- lib/page/settings_page.dart
+++ lib/page/settings_page.dart
@@ -48,7 +48,7 @@ class _SettingsPageState extends State<SettingsPage>
   List<String> _envList = ['生产', '测试'];
   String _selectedEnv = Cache.instance.inProduction ? '生产' : '测试';
 
-  String _verStr = 'Version 1.2.1908231416';
+  String _verStr = 'Version 1.5.1910211455';
 
   String get cacheKey => '__Cache.instance.site_list';
   @override
